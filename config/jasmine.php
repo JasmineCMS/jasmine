bd8be0f5a76b6a2e127949bdcd9360797a73ab63
Mordi Sacks <mordisacks@gmail.com>
2020-11-05 17:18:25 +0200

    fixed jasmine auth

--- config/jasmine.php
+++ config/jasmine.php
@@ -12,6 +12,6 @@ return [
 
     'auth' => [
         'guard'  => env('JASMINE_AUTH_GUARD', 'jasmine_web'),
-        'broker' => env('JASMINE_AUTH_GUARD', 'jasmine_users'),
+        'broker' => env('JASMINE_AUTH_BROKER', 'jasmine_users'),
     ],
 ];
