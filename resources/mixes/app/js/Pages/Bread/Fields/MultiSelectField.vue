3e87e7532d2058592b1bbcb7ba3641474a2099b3
Hadas Ben Mordechai <83160066+HadasBenMordechai@users.noreply.github.com>
2024-01-20 19:10:30 +0200

    Added multiselect field based on vueform-multiselect

---
+++ resources/mixes/app/js/Pages/Bread/Fields/MultiSelectField.vue
@@ -0,0 +1,32 @@
+<template>
+  <Multiselect
+      :id="id" :name="name" v-model="val"
+      :required="validation.indexOf('required') > -1"
+      class="form-control"
+      :class="{'is-invalid': invalid}"
+      v-bind="opts"/>
+</template>
+
+<script>
+import '@vueform/multiselect/themes/default.css';
+
+import Multiselect from '@vueform/multiselect';
+import JasmineBaseField from './BaseField';
+
+export default {
+  name: 'MultiSelectField',
+  extends: JasmineBaseField,
+  components: {Multiselect},
+  data() {
+    return {
+      opts: Object.assign({
+        options: [],
+      }, this.options),
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
