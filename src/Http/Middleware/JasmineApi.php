33d6a01be9772663a4b542585ce8dbb141b9c70d
Mordi Sacks <MordiSacks@users.noreply.github.com>
2024-07-22 09:18:01 +0300

    API rate auth rate limit

--- src/Http/Middleware/JasmineApi.php
+++ src/Http/Middleware/JasmineApi.php
@@ -5,6 +5,7 @@ namespace Jasmine\Jasmine\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
 use Jasmine\Jasmine\Models\JasmineUserApiToken;
 
 class JasmineApi
@@ -19,12 +20,15 @@ class JasmineApi
      */
     public function handle(Request $request, Closure $next)
     {
+        if (RateLimiter::tooManyAttempts('jasmine.api.auth.' . $request->ip(), 5)) abort(403);
+
         /** @var JasmineUserApiToken $token */
         $token = JasmineUserApiToken::firstWhere('token', $request->bearerToken());
-        if (!$token) abort(401);
 
-        // check if expired
-        if ($token->expires_at && $token->expires_at < now()) abort(401);
+        if (!$token || ($token->expires_at && $token->expires_at < now())) {
+            RateLimiter::increment('jasmine.api.auth.' . $request->ip(), 60 * 5);
+            abort(401);
+        }
 
         Auth::guard(config('jasmine.auth.guard'))->loginUsingId($token->jasmine_user_id);
 
