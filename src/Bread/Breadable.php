eccc736087a1a118878a16c88261829d0b0d838a
Mordi Sacks <13218192+MordiSacks@users.noreply.github.com>
2024-07-24 21:32:10 +0300

    improved breadable

--- src/Bread/Breadable.php
+++ src/Bread/Breadable.php
@@ -17,20 +17,24 @@ trait Breadable
 {
     public static function getBreadableKey(): string
     {
-        return str_replace('\\', '-', static::class);
+        return once(function () {
+            $k = str_replace('\\', '-', static::class);
+            if (str_starts_with($k, 'App-Models-')) $k = Str::of($k)->substr(11)->plural()->lower();
+            return $k;
+        });
     }
-
+    
     public static function getPluralName(): string
     {
-        return __(Str::title(Str::snake(Str::pluralStudly(class_basename(static::class)), ' ')));
+        return once(fn() => __(Str::title(Str::snake(Str::pluralStudly(class_basename(static::class)), ' '))));
     }
-
+    
     public static function getSingularName(): string
     {
-        return __(Str::singular(Str::title(Str::snake(class_basename(static::class), ' '))));
+        return once(fn() => __(Str::singular(Str::title(Str::snake(class_basename(static::class), ' ')))));
     }
-
+    
     public static function getMenuIcon(): string { return 'bi-files'; }
-
+    
     public function getTitle(): string { return (string)$this->getKey(); }
 }
