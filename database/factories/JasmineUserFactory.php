886cf3b3bb18e57a608be46c59ae9f84e8f38e72
Mordi Sacks <13218192+MordiSacks@users.noreply.github.com>
2024-08-01 21:46:19 +0300

    wip

--- database/factories/JasmineUserFactory.php
+++ database/factories/JasmineUserFactory.php
@@ -27,19 +27,15 @@ class JasmineUserFactory extends Factory
         return [
             'name'     => fake()->name(),
             'email'    => fake()->unique()->safeEmail(),
-            //'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             //'remember_token'    => Str::random(10),
         ];
     }
     
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function verified(): static
     {
         return $this->state(fn(array $attributes) => [
-            'email_verified_at' => null,
+            'email_verified_at' => now(),
         ]);
     }
 }
