3f46ea268fec7cc5815982579f437a0d333b32fb
Mordi Sacks <13218192+MordiSacks@users.noreply.github.com>
2024-08-01 21:54:36 +0300

    wip

--- src/Models/JasmineUser.php
+++ src/Models/JasmineUser.php
@@ -5,6 +5,7 @@ namespace Jasmine\Jasmine\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\DatabaseNotification;
 use Illuminate\Notifications\DatabaseNotificationCollection;
@@ -64,8 +65,9 @@ use Illuminate\Notifications\Notifiable;
  */
 class JasmineUser extends Authenticatable implements BreadableInterface
 {
+    use HasFactory;
     use Notifiable, Breadable;
-
+    
     protected $fillable = [
         'name',
         'email',
@@ -75,60 +77,60 @@ class JasmineUser extends Authenticatable implements BreadableInterface
         'password',
         'otp_secret',
     ];
-
+    
     protected $hidden = [
         'password',
         'otp_secret',
         'otp_remember_token',
         'remember_token',
     ];
-
+    
     protected $casts = [
         'admin'       => 'boolean',
         'roles'       => 'array',
         'permissions' => 'array',
     ];
-
+    
     public function avatarUrl(): Attribute
     {
         return Attribute::get(function () {
             $uiAvatar = 'https://ui-avatars.com/api/' . urlencode($this->name ?? 'X') . '/100/EBF4FF/7F9CF5';
-
+            
             if (!$this->email) return $uiAvatar;
-
+            
             return 'https://www.gravatar.com/avatar/' . md5(strtolower($this->email))
                 . '?d=' . urlencode($uiAvatar)
                 . '&s=100';
         });
     }
-
+    
     public function apiTokens(): HasMany
     {
         return $this->hasMany(JasmineUserApiToken::class);
     }
-
+    
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new ResetPasswordNotification($token));
     }
-
+    
     public function jCan(string $permission): bool
     {
         if ($this->id === 1 || $this->admin) return true;
-
+        
         if (!str_contains($permission, '*')) return in_array($permission, $this->permissions ?? []);
-
+        
         foreach ($this->permissions as $per) if (Str::is($permission, $per)) return true;
-
+        
         return false;
     }
-
+    
     public static function getBreadableKey(): string { return 'jasmine-users'; }
-
+    
     public static function getMenuIcon(): string { return 'bi-people'; }
-
+    
     public function getTitle(): string { return $this->name; }
-
+    
     public static function browseableColumns(): array
     {
         return [
@@ -144,14 +146,14 @@ class JasmineUser extends Authenticatable implements BreadableInterface
             ],
         ];
     }
-
+    
     public static function fieldsManifest(): FieldsManifest
     {
         /** @var JasmineUser $user */
         $user = func_get_arg(0);
         $unique = Rule::unique('jasmine_users', 'email');
         if ($user->exists) $unique->ignore($user->id);
-
+        
         return new FieldsManifest([
             'col-md-4'        => [
                 'Details' => [
@@ -170,31 +172,31 @@ class JasmineUser extends Authenticatable implements BreadableInterface
             ],
         ]);
     }
-
+    
     public static function jasmineOnRetrievedForEdit(JasmineUser $u): array
     {
         if (!$u->exists) return $u->toArray();
-
+        
         // prep permissions
         $u->permissions = Arr::undot(
             array_combine($u->permissions, array_map(fn() => true, $u->permissions))
             + array_map(fn() => false, Jasmine::getPermissions())
         );
-
+        
         return $u->toArray();
     }
-
+    
     public static function jasmineOnSaving(array $data, JasmineUser $u): array
     {
         if (!$u->exists) $data['password'] = bcrypt(Str::random(64));
-
+        
         // normalize permissions
         if ($u->id === 1) $data['admin'] = true;
         $data['permissions'] = array_keys(array_filter(Arr::dot($data['permissions'] ?? []), fn($p) => $p));
-
+        
         return $data;
     }
-
+    
     public static function jasmineOnDeleting(JasmineUser $u): void
     {
         // prevent deleting first user
@@ -203,7 +205,7 @@ class JasmineUser extends Authenticatable implements BreadableInterface
             'title' => 'Oops!',
             'text'  => 'First user cannot be deleted',
         ])->throwResponse();
-
+        
         // prevent self destruction
         if ($u->id === Auth::guard(config('jasmine.auth.guard'))->user()->id) redirect()->back()->withSwal([
             'icon'  => 'error',
@@ -211,5 +213,5 @@ class JasmineUser extends Authenticatable implements BreadableInterface
             'text'  => 'You Cannot delete yourself',
         ])->throwResponse();
     }
-
+    
 }
