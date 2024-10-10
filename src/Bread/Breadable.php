23525706037dd5855d18595ea011d51c2252bfd4
Mordi Sacks <MordiSacks@users.noreply.github.com>
2024-10-10 15:20:14 +0300

    fixed bread key name

--- src/Bread/Breadable.php
+++ src/Bread/Breadable.php
@@ -20,25 +20,25 @@ trait Breadable
     {
         return once(function () {
             $k = str_replace('\\', '-', static::class);
-            if (str_starts_with($k, 'App-Models-')) $k = Str::of($k)->substr(11)->plural()->lower();
+            if (str_starts_with($k, 'App-Models-')) $k = Str::of($k)->substr(11)->plural()->kebab()->lower();
             return $k;
         });
     }
-    
+
     public static function getPluralName(): string
     {
         return once(fn() => __(Str::title(Str::snake(Str::pluralStudly(class_basename(static::class)), ' '))));
     }
-    
+
     public static function getSingularName(): string
     {
         return once(fn() => __(Str::singular(Str::title(Str::snake(class_basename(static::class), ' ')))));
     }
-    
+
     public static function getMenuIcon(): string { return 'bi-files'; }
-    
+
     public function getTitle(): string { return (string)$this->getKey(); }
-    
+
     public static function fake(bool $all = false, array $override = []): array
     {
         return ManifestFaker::fake(static::class, $all, $override);
